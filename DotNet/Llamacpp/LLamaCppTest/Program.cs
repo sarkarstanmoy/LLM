87110bb166fb6c7b1bcd9bb0b78f7e2b05faf7ee
Tanmoy Sarkar <tanmoysarkar1@gmail.com>
2023-10-03 08:21:19 +0530

    python code for generating video

--- DotNet/Llamacpp/LLamaCppTest/Program.cs
+++ DotNet/Llamacpp/LLamaCppTest/Program.cs
@@ -1,5 +1,7 @@
 ﻿using LLama.Common;
 using LLama;
+using Azure.Messaging.WebPubSub;
+using static LLama.Common.ChatHistory;
 
 string modelPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "huggingface\\gguf\\models\\codellama-7b-instruct.Q2_K.gguf"); // change it to your own model path
 var prompt = "Transcript of a dialog, where the User interacts with an Assistant named Tanistha. Tanistha is helpful, kind, honest, good at writing, and never fails to answer the User's requests immediately and with precision.\r\n\r\nUser: Hello, Tanistha.\r\nTanistha: Hello. How may I help you today?\r\nUser:"; // use the "chat-with-bob" prompt here.
@@ -13,12 +15,15 @@ ChatSession session = new ChatSession(ex);
 // show the prompt
 Console.WriteLine();
 Console.Write(prompt);
-
+var connectionString = "";
+var hub = "myhub1";
 // run the inference in a loop to chat with LLM
 while (prompt != "stop")
 {
     foreach (var text in session.Chat(prompt, new InferenceParams() { Temperature = 0.6f, AntiPrompts = new List<string> { "User:" } }))
     {
+        var serviceClient = new WebPubSubServiceClient(connectionString, hub);
+        await serviceClient.SendToAllAsync(text);
         Console.Write(text);
     }
     prompt = Console.ReadLine();
